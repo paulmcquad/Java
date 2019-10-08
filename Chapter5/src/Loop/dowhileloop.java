6c47f38038c4d0eed00fded6fa71af3d1af581c1
Paul McQuade <paulmcquad@gmail.com>
2019-10-08 14:42:23 +0100

    Loop Control
    while,do..while,for
    break, continue

---
+++ Chapter5/src/Loop/dowhileloop.java
@@ -0,0 +1,14 @@
+package Loop;
+
+public class dowhileloop {
+
+	public static void main(String[] args) {
+		int x = 10;
+
+		do {
+			System.out.print("value of x : " + x );
+			x++;
+			System.out.print("\n");
+		}while( x < 20 );
+	}
+}
