e42c2b5fd54c89ca09325736e8c1f30c5f5874bb
Paul McQuade <paulmcquad@gmail.com>
2019-10-08 13:35:04 +0100

    Parameterized Constructors
    Declare int i for x

---
+++ Chapter2/src/ParaCon/ParaClass.java
@@ -0,0 +1,11 @@
+package ParaCon;
+
+//A simple constructor.
+public class ParaClass {
+	int x;
+
+	// Following is the constructor
+	ParaClass(int i ) {
+		x = i;
+	}
+}
---
+++ Chapter2/src/ParaCon/simplecon.java
@@ -0,0 +1,9 @@
+package ParaCon;
+
+public class simplecon {
+	public static void main(String args[]) {
+		ParaClass t1 = new ParaClass( 10 );
+		ParaClass t2 = new ParaClass( 20 );
+		System.out.println(t1.x + " " + t2.x);
+	}
+}
