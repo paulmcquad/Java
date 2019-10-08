d395abc5ed82b404757f19c584c291ae4606fce7
Paul McQuade <paulmcquad@gmail.com>
2019-10-08 14:26:11 +0100

    Java - Basic Operators
    Conditional and instanceof Operators

---
+++ Chapter4/src/BasicOp/basic.java
@@ -0,0 +1,15 @@
+// Java - Basic Operators
+package BasicOp;
+
+public class basic {
+
+	public static void main(String[] args) {
+		int a, b;
+		a = 10;
+		b = (a == 1) ? 20: 30;
+		System.out.println( "Value of b is : " +  b );
+
+		b = (a == 10) ? 20: 30;
+		System.out.println( "Value of b is : " + b );
+	}
+}
---
+++ Chapter4/src/BasicOp/bool.java
@@ -0,0 +1,14 @@
+// Java - Basic Operators
+package BasicOp;
+
+public class bool {
+
+	public static void main(String[] args) {
+
+		String name = "James";
+
+		// following will return true since name is type of String
+		boolean result = name instanceof String;
+		System.out.println( result );
+	}
+}
