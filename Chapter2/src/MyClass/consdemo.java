830b55a4cb9905658533cbe5592c650a72205468
Paul McQuade <paulmcquad@gmail.com>
2019-10-08 13:21:15 +0100

    No Argument Constructors
    Basic "myclass.java"

---
+++ Chapter2/src/MyClass/consdemo.java
@@ -0,0 +1,9 @@
+package MyClass;
+
+public class consdemo {
+	public static void main(String args[]) {
+		myclass t1 = new myclass();
+		myclass t2 = new myclass();
+		System.out.println(t1.num + " " + t2.num);
+	}
+}
---
+++ Chapter2/src/MyClass/myclass.java
@@ -0,0 +1,8 @@
+package MyClass;
+
+public class myclass {
+	int num;
+	myclass() {
+		num = 100;
+	}
+}
