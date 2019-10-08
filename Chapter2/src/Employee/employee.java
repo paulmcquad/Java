d6d76096ab4c60c31b10776271ec8057c3358bd0
Paul McQuade <paulmcquad@gmail.com>
2019-10-08 13:03:16 +0100

    "Employee" - 2 Classes
    Working with Objects and Classes

---
+++ Chapter2/src/Employee/employee.java
@@ -0,0 +1,44 @@
+/* WARNING: has not main function:
+ * Complie with employeetest.java 
+ * This is a basic class that needs
+ * employeetest to work.
+ * */
+package Employee;
+import java.io.*;
+
+public class employee {
+
+
+	String name;
+	int age;
+	String designation;
+	double salary;
+
+	// This is the constructor of the class Employee
+	public employee(String name) {
+		this.name = name;
+	}
+
+	// Assign the age of the Employee  to the variable age.
+	public void empAge(int empAge) {
+		age = empAge;
+	}
+
+	/* Assign the designation to the variable designation.*/
+	public void empDesignation(String empDesig) {
+		designation = empDesig;
+	}
+
+	/* Assign the salary to the variable	salary.*/
+	public void empSalary(double empSalary) {
+		salary = empSalary;
+	}
+
+	/* Print the Employee details */
+	public void printEmployee() {
+		System.out.println("Name:"+ name );
+		System.out.println("Age:" + age );
+		System.out.println("Designation:" + designation );
+		System.out.println("Salary:" + salary);
+	}
+}
---
+++ Chapter2/src/Employee/employeetest.java
@@ -0,0 +1,21 @@
+package Employee;
+
+public class employeetest {
+
+	public static void main(String args[]) {
+		/* Create two objects using constructor */
+		employee empOne = new employee("James Smith");
+		employee empTwo = new employee("Mary Anne");
+
+		// Invoking methods for each object created
+		empOne.empAge(26);
+		empOne.empDesignation("Senior Software Engineer");
+		empOne.empSalary(1000);
+		empOne.printEmployee();
+
+		empTwo.empAge(21);
+		empTwo.empDesignation("Software Engineer");
+		empTwo.empSalary(500);
+		empTwo.printEmployee();
+	}
+}
