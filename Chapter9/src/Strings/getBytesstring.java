4b67fafdbe659170ecd4b47ed48ea2787a7ad55f
Paul McQuade <paulmcquad@gmail.com>
2019-10-10 11:48:39 +0100

    Strings Chapter 9
    Sr.No. 16

---
+++ Chapter9/src/Strings/getBytesstring.java
@@ -0,0 +1,18 @@
+package Strings;
+import java.io.*;
+
+public class getBytesstring {
+
+	public static void main(String args[]) {
+		String Str1 = new String("Welcome to Tutorialspoint.com");
+
+		try {
+			String Str2 = new String( Str1.getBytes( "UTF-8" ));
+			System.out.println("Returned Value " + Str2 );
+			Str2 = new String (Str1.getBytes( "ISO-8859-1" ));
+			System.out.println("Returned Value " + Str2 );
+		} catch ( UnsupportedEncodingException e) {
+			System.out.println("Unsupported character set");
+		}
+	}
+}
---
+++ Chapter9/src/Strings/hashCodestring.java
@@ -0,0 +1,9 @@
+package Strings;
+
+public class hashCodestring {
+
+	public static void main(String args[]) {
+		String Str = new String("Welcome to Tutorialspoint.com");
+		System.out.println("Hashcode for Str :" + Str.hashCode() );
+	}
+}
